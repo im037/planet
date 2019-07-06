b30b00fdde753692f548c2e3bd8d08f3a3919fd5
Leonardo Souza <leonardo_m7@hotmail.com>
2019-07-05 22:02:14 -0300

    create class model to get results for swapi

---
+++ src/main/java/com/rest/planet/model/SwapiResponse.java
@@ -0,0 +1,44 @@
+package com.rest.planet.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class SwapiResponse {
+
+    private Integer count;
+    private String next;
+    private String previous;
+    private List<SwapiPlanet> results;
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public String getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(String previous) {
+        this.previous = previous;
+    }
+
+    public List<SwapiPlanet> getResults() {
+        return results;
+    }
+
+    public void setResults(List<SwapiPlanet> results) {
+        this.results = results;
+    }
+}
