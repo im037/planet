350d6872687d228a5e5b6ed5065c41489a00163a
Leonardo Souza <leonardo_m7@hotmail.com>
2019-07-06 09:05:40 -0300

    exclude auto config mongodb

--- src/main/java/com/rest/planet/PlanetApplication.java
+++ src/main/java/com/rest/planet/PlanetApplication.java
@@ -2,8 +2,10 @@ package com.rest.planet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class PlanetApplication {
 
     public static void main(String[] args) {
