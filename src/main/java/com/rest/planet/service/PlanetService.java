b0274d2e5a9bc7d4ed66e721a4f545ab5d39212e
Leonardo Souza <leonardo@ipremios.com>
2019-07-08 20:03:52 -0300

    refactor all methods in PlanetService

--- src/main/java/com/rest/planet/service/PlanetService.java
+++ src/main/java/com/rest/planet/service/PlanetService.java
@@ -1,8 +1,15 @@
 package com.rest.planet.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.rest.planet.domain.Planet;
+import com.rest.planet.repository.PlanetRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -11,24 +18,59 @@ public class PlanetService {
     @Autowired
     private ApparitionsService apparitionsService;
 
-    public Map addPlanet(Map body) {
-        body.put("apparitions", apparitionsService.countApparitionsByPlanet((String) body.get("nome")));
-        return body;
+    @Autowired
+    PlanetRepository planetRepository;
+
+    public Planet createPlanet(Map bodyMap) throws IOException {
+        bodyMap.put("apparitions", apparitionsService.countApparitionsByPlanet((String) bodyMap.get("nome")));
+
+        translateFields(bodyMap);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(bodyMap);
+
+        ObjectMapper mapper = new ObjectMapper();
+        Planet planet = mapper.readValue(json, Planet.class);
+
+        return planetRepository.save(planet);
     }
 
-    public String findAll() {
-        return "Listar planetas";
+    public List<Planet> findAll() {
+        return planetRepository.findAll();
     }
 
-    public String findByName(String name) {
-        return "Buscar por nome - " + name;
+    public Planet findByName(String name) {
+        return planetRepository.findByName(name);
     }
 
-    public String findById(Integer planetId) {
-        return "Buscar por ID - " + planetId;
+    public Planet findById(ObjectId planetId) {
+        return planetRepository.findById(planetId);
     }
 
-    public String deletePlanet() {
-        return "Remover planeta";
+    public Planet findById(String planetId) {
+        return findById(new ObjectId(planetId));
+    }
+
+    public Planet deletePlanet(String name) {
+        Planet planet = planetRepository.findByName(name);
+        if(planet != null) {
+            planetRepository.delete(planet);
+        }
+        return planet;
+    }
+
+    public void translateFields(Map body){
+        if(body.containsKey("nome")){
+            body.put("name", body.get("nome"));
+            body.remove("nome");
+        }
+        if(body.containsKey("clima")){
+            body.put("climate", body.get("clima"));
+            body.remove("clima");
+        }
+        if(body.containsKey("terreno")){
+            body.put("terrain", body.get("terreno"));
+            body.remove("terreno");
+        }
     }
 }
